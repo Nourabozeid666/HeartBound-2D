173dcac713dfd003b31b68865db295b86edf54c4
SalmaMahmoud44 <121463290+SalmaMahmoud44@users.noreply.github.com>
2025-09-06 16:28:51 +0300

    ...

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletDestroyingTime;
+    [SerializeField] float speed;
+    private Animator animator;
+    [SerializeField] float BulletDamage;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        StartCoroutine(bulletSystem());
+       // rb.linearVelocity = Vector2.up * speed;
+    }
+    IEnumerator bulletSystem()
+    {
+        yield return new WaitForSeconds(1f);
+        animator.SetBool("active", true);
+        yield return new WaitForSeconds(0.4f);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            EnemyHealth hitEnemy = collision.GetComponent<EnemyHealth>();
+            if (hitEnemy != null)
+            {
+                hitEnemy.TakeDamage(BulletDamage);
+            }
+            Destroy(gameObject);
+        }
+    }
+    }
---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float health;
+    float currentHealth;
+    public bool isDead = false;
+    [SerializeField] Animator animator;
+    [SerializeField] AudioClip enemyHurt;
+    [SerializeField] AudioClip enemyDead;
+    private bool recentlyHit = false;
+    void Start()
+    {
+        currentHealth = health;
+    }
+    public void TakeDamage(float damage)
+    {
+        if (isDead || recentlyHit) return;
+        StartCoroutine(HitCooldown());
+        currentHealth -= damage;
+        if (currentHealth < 0)
+            currentHealth = 0;
+        if (currentHealth == 0)
+        {
+            //AudioManager.instance.EnemyAction(enemyDead);
+            animator.SetBool("isDead", true);
+            isDead = true;
+            StartCoroutine(DeathCleanup());
+        }
+        else
+        {
+            //AudioManager.instance.EnemyAction(enemyHurt);
+            HitReactionRoutine();
+        }
+    }
+    //to avoid the double hit from the back
+    IEnumerator HitCooldown()
+    {
+        recentlyHit = true;
+        yield return new WaitForSeconds(0.4f); // adjust based on animation/delay
+        recentlyHit = false;
+    }
+    void HitReactionRoutine()
+    {
+        StartCoroutine(EnemyHitReaction());
+    }
+    IEnumerator EnemyHitReaction()
+    {
+        animator.SetBool("isHurt", true);
+
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        animator.SetBool("isHurt", false);
+    }
+    IEnumerator DeathCleanup()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
+}
